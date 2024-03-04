adbdbeb43e9a4b58e60a6204b17f8dd162b49b60
Ben Fashan <benfashan71@msn.com>
2024-03-04 00:50:13 +0000

    ADD: Recipe List

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Recipe
+from .forms import RecipeForm
 
 # Create your views here.
+
+class Recipes(ListView):
+    """
+    view for all recipes
+    """
+    template_name = 'recipes/recipes.html'
+    model = Recipe
+    context_object_name = 'recipes'
+
+
+
+
+class AddRecipe(LoginRequiredMixin, CreateView):
+    """
+    Add recipe view
+    """
+    template_name = 'recipes/add_recipe.html'
+    model = Recipe
+    form_class = RecipeForm
+    success_url = '/recipes/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
